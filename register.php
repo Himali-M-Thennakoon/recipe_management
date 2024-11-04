6d326d4eaeac53026f0c6e2061f58794e894a101
Sanjeewa Liyanage <138761214+Sanjeewa-Liyanage@users.noreply.github.com>
2024-11-04 10:38:34 +0530

    Update register.php

--- register.php
+++ register.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MixBowls Registration</title>
+    <title>Recipe Management System Registration</title>
     <link rel="stylesheet" href="login_register.css">
 </head>
 <body>
