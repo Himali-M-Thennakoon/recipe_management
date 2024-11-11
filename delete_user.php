27610d1873641e25843c9794590166e22bd6b277
Sanjeewa Liyanage <138761214+Sanjeewa-Liyanage@users.noreply.github.com>
2024-11-12 03:03:56 +0530

    updated delete user

--- delete_user.php
+++ delete_user.php
@@ -4,7 +4,7 @@ include 'config.php';
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
-    // Check if the user is an admin
+    
     $checkAdminSql = "SELECT type FROM users WHERE id = $id";
     $result = $conn->query($checkAdminSql);
     
@@ -12,14 +12,14 @@ if (isset($_GET['id'])) {
         $row = $result->fetch_assoc();
         
         if ($row['type'] == 'admin') {
-            // Admin deletion error message
+           
             echo "<script type='text/javascript'>
                     alert('You cant delete admin from the System.');
                     window.location.href = 'listusers.php';
                   </script>";
             exit;
         } else {
-            // Proceed with deletion for non-admin users
+            
             $sql = "DELETE FROM users WHERE id = $id";
             
             if ($conn->query($sql) === TRUE) {
